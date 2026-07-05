160f0e26fff6c604c96a44f496bdf894900d173b
QuantumMisaka <ff6757442@gmail.com>
2026-07-05 16:57:24 +0800

    test: pin ATST banner output

--- tests/unit/test_banner.py
+++ tests/unit/test_banner.py
@@ -5,8 +5,18 @@ from atst_tools.utils.banner import ATST_ASCII, BANNER_CREDITS, render_banner
 
 def test_render_banner_contains_ascii_atst_and_contributor_credits():
     """The banner should include the ATST art and governed credit lines."""
+    expected_banner = (
+        "    _  _____ ____ _____\n"
+        "   / \\|_   _/ ___|_   _|\n"
+        "  / _ \\ | | \\___ \\ | |\n"
+        " / ___ \\| |  ___) || |\n"
+        "/_/   \\_\\_| |____/ |_|\n"
+        "Core developer: @QuantumMisaka\n"
+        "Contributors: @Jerry, @MoseyQAQ, and the ATST-Tools contributors\n"
+    )
     banner = render_banner()
 
+    assert banner == expected_banner
     assert ATST_ASCII in banner
     assert BANNER_CREDITS in banner
     assert "Core developer: @QuantumMisaka" in banner
